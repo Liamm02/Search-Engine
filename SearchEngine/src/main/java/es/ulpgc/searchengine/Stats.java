d5d0990b7408fde02dc2d75d861fbd47fa07d046
Krish Sadhwani <92883393+KrishT97@users.noreply.github.com>
2023-01-03 14:56:38 +0000

    Fixed many errors
    Removed unnecessary fields, exceptions calling, use of primitive variables, enhancing for loop

--- SearchEngine/src/main/java/es/ulpgc/searchengine/Stats.java
+++ SearchEngine/src/main/java/es/ulpgc/searchengine/Stats.java
@@ -14,12 +14,9 @@ import static es.ulpgc.searchengine.ApiController.solveApiConsult;
 
 
 public class Stats {
-    private static Integer oldestbook =0;
-    private static String book;
-    private static Map<String,Integer> resultado = new HashMap<>();
-    public static String getOldestBook(String path) throws IOException, ParseException, InterruptedException {
+    public static String getOldestBook() throws IOException {
         Map<String, String> bookDate = new HashMap<>();
-        ArrayList<String> metadataCoincidences = new ArrayList<>();
+        ArrayList<String> metadataCoincidences;
         for (int i = 1450; i < Year.now().getValue(); i++) {
             bookDate.put("from", String.valueOf(i));
             bookDate.put("to", String.valueOf(i));
@@ -34,7 +31,7 @@ public class Stats {
     }
     public static Map<String, Integer> getMostCommonWord() throws IOException, ParseException {
         String word  = "";
-        Integer count = 0;
+        int count = 0;
         Map<String,Integer> result = new HashMap<>();
         JSONParser parser = new JSONParser();
         File dir = new File("./Datamart/Inv_Index.json");
@@ -42,16 +39,16 @@ public class Stats {
         JSONObject jsonObject = (JSONObject) obj;
         for (Object key : jsonObject.keySet()){
                 JSONArray array = (JSONArray) jsonObject.get(key);
-                for (int i = 0; i < array.size(); i++) {
-                    JSONObject object = (JSONObject) array.get(i);
-                    for (Object k : object.keySet()) {
-                        JSONArray ar = (JSONArray) object.get(k);
-                        if (ar.size() > count) {
-                            count = ar.size();
-                            word = String.valueOf(key);
-                        }
+            for (Object o : array) {
+                JSONObject object = (JSONObject) o;
+                for (Object k : object.keySet()) {
+                    JSONArray ar = (JSONArray) object.get(k);
+                    if (ar.size() > count) {
+                        count = ar.size();
+                        word = String.valueOf(key);
                     }
                 }
+            }
 
         }
         result.put(word,count);
